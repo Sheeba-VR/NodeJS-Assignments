b6b8637a1cf01ed2c208fbb2b839aefa39f664f2
Sheeba-VR <124991013+Sheeba-VR@users.noreply.github.com>
2023-02-10 12:14:14 +0530

    Add files via upload

---
+++ Second-Assignment/ExportFunction.js
@@ -0,0 +1,4 @@
+export function SimpleInterest(p,n,r) {
+let interest=(p*n*r)/100;
+return interest
+}
---
+++ Second-Assignment/MoneyInterestCalculator.js
@@ -0,0 +1,6 @@
+import {SimpleInterest} from "./ExportFunction.js"
+var cap=50000
+var year=1
+var rate=8
+var interest=SimpleInterest(cap,year,rate)
+console.log(interest)
