3cf129105bd4d6702e8367035c447d4e94820b46
Sheeba-VR <124991013+Sheeba-VR@users.noreply.github.com>
2023-02-10 12:10:20 +0530

    Add files via upload

---
+++ First Assignment/First.js
@@ -0,0 +1,13 @@
+let capital=60000
+let year=5
+let rate=8
+let interest=(capital*year*rate)/100
+console.log(interest)
+let name2="Sheeba"
+let chechk=true
+let mark=[12,23,43,54]
+console.log(name2)
+console.log(chechk)
+console.log(mark)
+let obj=[{name1:"SS",mark:100},{name1:true,mark:10}]
+console(obj)
