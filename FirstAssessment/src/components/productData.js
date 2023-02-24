358a99b5468e27d8a89e15c2c7ac33da4368fee5
Sheeba-VR <124991013+Sheeba-VR@users.noreply.github.com>
2023-02-24 17:06:58 +0530

    Add files via upload

---
+++ FirstAssessment/src/components/productData.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import Product from './Product';
+import './Product.css';
+export class ProductData extends Component {
+    products = [
+        { "title":"Brown Egg",
+          "type":"Dairy",
+          "description":"Raw organic brown eggs in a basket",
+          "url":"http://wovenmeadows.com/wordpress/wp-content/uploads/2012/10/Valdale-Farm-Eggs-1005.jpg",
+          "price":"$9.50",
+          "rating":4 
+        },
+           {
+               "title":"Milk",
+               "type":"Dairy",
+               "description":"Homely Item",
+               "url":"https://tse1.mm.bing.net/th/id/OIP.2uw1ufzt3eqSZuf0tX_SNQHaHa?w=187&h=187&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+               "price":"$27.50",
+               "rating":5
+            }, 
+            {
+                "title":"SoftDrinks",
+                 "type":"Drinks",
+                 "description":"Instant Drink",
+                 "url":"https://tse2.mm.bing.net/th/id/OIP.oG2V25yg3Nsh_6MX-ja_wAHaEm?w=295&h=184&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+                 "price":"$27.50",
+                 "rating":5
+                 }, 
+                 {
+                      "title":"Sweet fresh stawberry",
+                      "type":"Fruit",
+                      "description":"Sweet fresh stawberry on the wooden table",
+                      "url":"https://tse2.mm.bing.net/th/id/OIP.OmSz410prsUB_-kVibYV4AHaE8?pid=ImgDet&rs=1",
+                      "price":"$120.00",
+                      "rating":5
+                     }
+                     ]
+                     constructor(){
+                         super();
+                         this.state = {products : this.state}
+                         } render() { 
+                             return (
+                                 <div>
+                                  {
+                                      this.products.map((element) => {
+                                      return <Product title={element.title} url={element.url} description={element.description}/> 
+                                      })
+                                 }
+                                 </div>
+                                  )
+                                 }}export default ProductData
